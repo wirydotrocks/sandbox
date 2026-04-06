48ad0c9dbcbe295b1dd63ee0e01d51a2a1c74de0
Darren <darViloria04@gmail.com>
2026-04-06 17:22:36 -0400

    Completed Card that Grows

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ModeToggle } from '@/components/themetoggle';
+import { CardGrower } from '@/components/cardgrower';
 
 
 export default function Home() {
@@ -8,10 +9,11 @@ export default function Home() {
 
   return (
     <div 
-      className="flex min-h-svh items-center justify-center font-mono grid-cols-5" 
+      className="flex min-h-svh items-center justify-center font-mono grid-cols-4 grid-rows-4 gap-4" 
     >
 
       <ModeToggle/>
+      <CardGrower/>
       
 
     </div>
---
+++ components/cardgrower.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
+
+export function CardGrower(){
+  const minHeight = 60
+  const [ size, setSize ] = useState(minHeight)
+
+  return(
+    <Card 
+      className="mx-auto w-full max-w-sm border-2 min-h-30"
+    >
+      <CardHeader>  
+        <CardTitle>This Card Grows </CardTitle>
+        <CardDescription>
+          I learned how to edit the parent of a button, and I used an array destructor.
+          Also, I learned how to use inline css to be changed by onClicks.
+        </CardDescription>
+      </CardHeader>
+
+      <CardFooter
+        style={{
+          height: size,
+        }}
+        className="flex gap-4"
+      > 
+        <Button 
+          style={{
+            height: size - 30,
+          }}
+          onClick={() => setSize((prev) => prev+10)}
+          className="flex-1"
+        >
+          beeg
+        </Button>
+
+        <Button 
+           style={{
+            height: size - 30,
+          }}
+         onClick={() => setSize((prev) => Math.max(minHeight, prev-10))}
+          className="flex-1"
+        >
+          smal 
+        </Button>
+ 
+      </CardFooter>
+
+
+    </Card>
+
+
+
+
+  )
+
+}
