9d7c89f6cc3b4482c2e84340b81c4f1b6ebc9624
Darren <darViloria04@gmail.com>
2026-04-08 18:23:25 -0400

    Attempt for modularity

--- components/personabutton.tsx
+++ components/personabutton.tsx
@@ -47,33 +47,43 @@ const textVariants = {
   },
 }
 
-function SkillButton({ label, className }) {
+function MenuButton({ label, className = "", textClassName = "", pinkClassName = "", whiteClassName = "", }) {
   const [hovered, setHovered] = useState(false)
 
   return (
-    <div className={`absolute -rotate-20 ${className}`}>
+    <div className={`absolute ${className}`}>
       <motion.span
+
+
         variants={pinkVariants}
+
         animate={hovered ? "hover" : "rest"}
-        className="pointer-events-none absolute z-10 inset-0 bg-[#ff93e2]
+
+        className={`pointer-events-none absolute z-10 bg-[#ff93e2]
                   [clip-path:polygon(100%_44%,4%_44%,88%_67%)]
-                  rotate-3.4"
+                  -rotate-20 ${pinkClassName} inset-0`}
       />
       <motion.span
+
         variants={whiteVariants}
+
         animate={hovered ? "hover" : "rest"}
-        className="pointer-events-none absolute z-20 inset-0 bg-[#ffffff]
+
+        className={`pointer-events-none absolute z-20 bg-[#ffffff]
                   [clip-path:polygon(100%_44%,4%_44%,88%_67%)]
-                  -rotate-4"
+                  -rotate-24 ${whiteClassName} inset-0`}
       />
       <motion.span
+
         onHoverStart={() => setHovered(true)}
         onHoverEnd={() => setHovered(false)}
+
         animate={hovered ? "hover" : "rest"}
         variants={textVariants}
-        className="flex items-center justify-center z-30 absolute
-        italic font-bold font-[P3R] text-5xl tracking-[-.1em]
-        translate-x-13 translate-y-22"
+
+        className={`flex items-center justify-center z-30 absolute
+        italic font-bold font-[P3R] tracking-[-.1em]
+         -rotate-20 ${textClassName}`}
       >
         {label}
       </motion.span>
@@ -100,11 +110,23 @@ export function PersonaButton() {
       <CardContent className="flex items-center justify-center">
 
         <div className="relative h-60 w-full"
-             style={{ isolation: 'isolate '}}
+              style={{ background: 'multiply' }}
         >
           <span className="absolute inset-0 -inset-x-6 bg-gradient-to-t from-[#2445ff] via-[#4275ff]"/>
-          
-          <SkillButton label="SKILL" className="inset-0"/>
+           
+          <MenuButton label="SKILL" 
+             className="w-full h-full top-0"
+             textClassName="translate-x-13 translate-y-22 text-5xl"
+             pinkClassName=""
+             whiteClassName=""
+          />
+
+          <MenuButton label="ITEM" 
+             className="w-full h-full top-14"
+             textClassName="translate-x-13 translate-y-22 text-2xl"
+             pinkClassName=""
+             whiteClassName=""
+          />
 
         </div>
 
