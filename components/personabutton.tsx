1b3467e1f96376b384461368b6ad1eb02ecb67b1
Darren <darViloria04@gmail.com>
2026-04-07 21:18:57 -0400

    Added SKILL shapes, color transitions

--- components/personabutton.tsx
+++ components/personabutton.tsx
@@ -3,18 +3,40 @@
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { motion } from "motion/react"
 
 
 export function PersonaButton(){
 
+   const pinkVariants = {
+      rest: { opacity: 0, transition: { duration: 0, delay: 0 }},
+      hover: { opacity: 1, transition: { delay: .04 }},
+    }
+
+   const whiteVariants = {
+      rest: { opacity: 0, transition: { duration: 0, delay: 0 }},
+      hover: { opacity: 1},
+    }
+
+   const textVariants = {
+     rest: { color: "#80CAFF", mixBlendMode: "normal"},
+     hover: { color: "red", mixBlendMode: "multiply"},
+   }
+
+
   return(
+
+
+
+
+
     <Card 
       className="mx-auto w-full max-w-sm border-2 min-h-30"
     >
       <CardHeader>  
         <CardTitle>Persona 3 Reload Select</CardTitle>
         <CardDescription>
-          Template Card...
+          Made with Motion! Hover it to see the effects.
         </CardDescription>
 
         <CardAction>
@@ -23,9 +45,69 @@ export function PersonaButton(){
 
       </CardHeader>
 
-      <CardContent>
+      <CardContent
+        className="flex items-center justify-center"
+      >
 
-          </CardContent>
+
+        <motion.button
+
+          initial="rest"
+          whileHover="hover"
+          
+          className="relative h-60 w-full"
+
+        >
+
+
+          <div
+            className="absolute inset-0 -rotate-20"
+          >
+
+          <span
+             className="absolute inset-0 -inset-x-6 z-0 rotate-20
+                        bg-gradient-to-t from-[#2445ff] via-[#4275ff]"
+          />
+
+
+          <motion.span
+
+            variants={pinkVariants}
+            transition={{ duration: 0}}
+
+            className="pointer-events-none absolute z-10 inset-0 bg-[#ff93e2]
+                      [clip-path:polygon(100%_44%,4%_44%,88%_67%)]
+                      rotate-2.4"
+
+          />
+
+          <motion.span
+
+            variants={whiteVariants}
+            transition={{ duration: 0}}
+
+            className="pointer-events-none absolute z-20 inset-0 bg-[#ffffff]
+                      [clip-path:polygon(100%_44%,4%_44%,88%_67%)]
+                      -rotate-4"
+
+          />
+
+          <motion.span
+
+            variants={textVariants}
+            transition={{ duration: .2 }}
+
+            className="flex items-center justify-center z-30 absolute inset-0 
+            italic font-bold font-[P3R] text-6xl tracking-[-.1em]"
+          >
+            SKILL
+          </motion.span>
+
+          </div>
+          
+        </motion.button>
+
+      </CardContent>
 
       <CardFooter
         className="flex gap-4"
